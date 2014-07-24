996cbc921e8a26cca85587464c91ad2a5eb67665
Steve Patterson <spatte21@btinternet.com>
2014-07-24 18:20:24 +0100

    Added a basic venue list page

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,50 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('drinkon', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.controller('VenueListCtrl', function($scope) {
+
+  $scope.venues = [
+    {
+      name: 'Cross Keys',
+      distance: 400,
+      sells: {
+        food: true,
+        drink: true
+      },
+      image: 'venue_1.jpg'
+    },
+    {
+      name: 'The Greyhound',
+      distance: 600,
+      sells: {
+        food: false,
+        drink: true
+      },
+      image: 'venue_2.jpg'
+    },
+    {
+      name: 'The Windmill',
+      distance: 1400,
+      sells: {
+        food: true,
+        drink: true
+      },
+      image: 'venue_3.jpg'
+    },
+    {
+      name: 'Dunnington Sports Club',
+      distance: 950,
+      sells: {
+        food: true,
+        drink: true
+      },
+      image: 'venue_4.jpg'
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  ];
+
+  $scope.hey = function(venue) {
+    alert('you want to go to the ' + venue.name + '?');
+  }
+});
