4c24a4d145dc587b233837464f981778ca38617c
Steve Patterson <spatte21@btinternet.com>
2014-10-17 17:17:43 +0100

    more changes

--- www/js/notification/notificationSvc.js
+++ www/js/notification/notificationSvc.js
@@ -1,3 +1,16 @@
-/**
- * Created by steve on 17/10/2014.
- */
+angular.module('drinkon').factory('notificationSvc', ['apiRoot', '$http', '$q', function(apiRoot, $http, $q) {
+  return {
+    getNotificationsForUser: function (userId) {
+      var defer = $q.defer();
+
+      $http.get(apiRoot + '/notification')
+        .then(function (result) {
+          defer.resolve(result.data.notifications);
+        }, function (err) {
+          defer.reject(err);
+        });
+
+      return defer.promise;
+    }
+  };
+}]);
